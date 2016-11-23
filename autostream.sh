244046181a549887f89f150be72b949178135ca6
bcarson7 <bpc31@msn.com>
2016-11-23 10:58:13 -0700

    Update autostream.sh

--- autostream.sh
+++ autostream.sh
@@ -1,4 +1,6 @@
-for i in {1..24}
+export OMP_NUM_THREADS=1
+./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' > stream.log
+for i in {2..24}
 do
 export OMP_NUM_THREADS=$i
 ./stream | grep 'Threads\|Copy\|Scale\|Add\|Triad' >> stream.log
